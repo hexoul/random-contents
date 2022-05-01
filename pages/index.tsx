9a67871519fb268497826d002fbc1a12c2fc1185
hexoul <crosien@gmail.com>
2022-05-01 12:00:13 +0900

    Clean up with lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
+  <Layout title="영화 & 드라마 무작위 추천">
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
